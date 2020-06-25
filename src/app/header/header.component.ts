cd55a0d189e67b64f158c1d819161ab1f3250bb5
anugya ram <anugyaram08@gmail.com>
2020-06-25 23:29:52 +0530

    next corousel

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,41 +1,13 @@
-import { Component, OnInit, ViewChild, HostListener, ElementRef } from '@angular/core';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { LoginComponent } from '../login/login.component';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent{
 
-  sticky: boolean = false;
-  elementPosition: any;
-
-  constructor(public dialog: MatDialog ) { }
-
-  @ViewChild('stickyMenu') menuElement: ElementRef;
-
-  menuPosition: any;
-  ngAfterViewInit(){
-    this.elementPosition = this.menuElement.nativeElement.offsetTop;
-  }
-
-
-@HostListener('window:scroll', ['$event'])
-    handleScroll(){
-      const windowScroll = window.pageYOffset;
-      if(windowScroll >= this.elementPosition){
-        this.sticky = true;
-      } else {
-        this.sticky = false;
-      }
-    }
-  ngOnInit() {
-  }
-
-  openLoginForm() {
-    this.dialog.open(LoginComponent, {width: '500px', height: '450px'});
-  }
+  constructor( ) { }
 
 }
