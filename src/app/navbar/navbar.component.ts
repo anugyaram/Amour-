a22a6d641e0467fffc01028f18fd13f5899b961e
anugya ram <anugyaram08@gmail.com>
2020-06-25 20:19:31 +0530

    responsive navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit,ViewChild, HostListener, ElementRef} from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-navbar',
@@ -10,8 +12,37 @@ import { map } from 'rxjs/operators';
 })
 export class NavbarComponent {
 
+  sticky: boolean = false;
+  elementPosition: any;
+
+
   isHandset: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset);
     
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, public dialog: MatDialog) {}
   
+  @ViewChild('stickyMenu') menuElement: ElementRef;
+
+  menuPosition: any;
+  ngAfterViewInit(){
+    this.elementPosition = this.menuElement.nativeElement.offsetTop;
+  }
+
+
+@HostListener('window:scroll', ['$event'])
+    handleScroll(){
+      const windowScroll = window.pageYOffset;
+      if(windowScroll >= this.elementPosition){
+        this.sticky = true;
+      } else {
+        this.sticky = false;
+      }
+    }
+  ngOnInit() {
+  }
+
+  openLoginForm() {
+    this.dialog.open(LoginComponent, {width: '500px', height: '450px'});
+  }
+
+
   }
